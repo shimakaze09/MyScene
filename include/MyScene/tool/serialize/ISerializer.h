c478ca4b011c436cf81afdac09d9b46344599ef2
John <yangtianzhuo970425@gmail.com>
2024-12-30 15:26:47 +1300

    Add JSON serializer implementation and update CMake configurations

--- include/MyScene/tool/serialize/ISerializer.h
+++ include/MyScene/tool/serialize/ISerializer.h
@@ -1,3 +1,20 @@
 //
 // Created by Admin on 30/12/2024.
 //
+
+#pragma once
+
+#include <MyDP/Reflection/ReflTraits.h>
+
+#include <string>
+
+namespace My {
+class Scene;
+class SObj;
+
+class ISerializer : public ReflTraitsVisitor {
+ public:
+  virtual std::string Serialize(const Scene* scene) = 0;
+  virtual std::string Serialize(const SObj* sobj) = 0;
+};
+}  // namespace My
